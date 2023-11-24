fe0e9a2357962670b7d41b4e86ac65376f2e7317
asiedujosh <asiedujosh@yahoo.com>
2023-11-24 06:49:50 +0000

    Change Login Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,6 @@
 "use client"
-import { useRouter } from "next/router"
-import { useEffect } from "react"
+import Login from "@/app/login/page"
 
 export default function Home() {
-  useEffect(() => {
-    const router = useRouter()
-    router.push("/sample")
-  }, [])
-
-  return <div className="text-white text-lg">Welcome to Bvs</div>
+  return <Login />
 }
