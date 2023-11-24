b77f2160339b3f55922a3bf65bd1bb5bfa101b7e
asiedujosh <asiedujosh@yahoo.com>
2023-11-24 06:22:31 +0000

    Changed navigation routes

--- src/app/context/Staff/StaffContextApi.js
+++ src/app/context/Staff/StaffContextApi.js
@@ -1,6 +1,6 @@
 "use client"
-import React, { createContext, useContext, useState } from "react"
-import { useRouter } from "next/router"
+import React, { createContext, useState } from "react"
+import { useRouter } from "next/navigation"
 import { notify } from "@/app/utils/responseUtils"
 import { SUCCESS_STATUS } from "../../constant/requestConstants"
 import {
