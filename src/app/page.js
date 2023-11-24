57eca8928aff31003f17c6b7c37a9d35042dbfda
asiedujosh <asiedujosh@yahoo.com>
2023-11-24 06:31:05 +0000

    Add test page

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { useEffect } from "react"
 export default function Home() {
   useEffect(() => {
     const router = useRouter()
-    router.push("/login")
+    router.push("/sample")
   }, [])
 
   return <div className="text-white text-lg">Welcome to Bvs</div>
---
+++ src/app/sample/page.js
@@ -0,0 +1,8 @@
+"use client"
+export default function Sample() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between py-24">
+      <h2 className="text-white text-lg">This is a sample page</h2>
+    </main>
+  )
+}
