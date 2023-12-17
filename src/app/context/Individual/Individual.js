26b670acacca3fbcdbaac87ec335774113f6eade
asiedujosh <asiedujosh@yahoo.com>
2023-12-17 16:21:46 +0000

    Debuging messages

--- src/app/context/Individual/Individual.js
+++ src/app/context/Individual/Individual.js
@@ -172,7 +172,7 @@ export const getService = async () => {
 export const sendMessage = async (tel, sms) => {
   try {
     let responseOnMessage = await axios.get(
-      `https://sms.arkesel.com/sms/api?action=send-sms&api_key=UVJKUnRwbXBqa3RjRHRqc0h2d0o=&to=${tel}&from=BVS&sms=${sms}`
+      `https://sms.arkesel.com/sms/api?action=send-sms&api_key=RWRFa3pxenNld2hvdlNiTGh0UHU&to=${tel}&from=BVS&sms=${sms}`
     )
     if (responseOnMessage.code === "ok") {
       return responseOnMessage.data
