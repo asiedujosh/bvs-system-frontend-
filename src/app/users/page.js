f7179f4c00ef5a4e2e4809404993ab311bd53ee0
asiedujosh <asiedujosh@yahoo.com>
2024-02-14 08:30:02 +0000

    Working on user

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -18,13 +18,24 @@ const Users = () => {
 
   return (
     <>
-      {singleUserPermission &&
+      {singleUserPermission ? (
         singleUserPermission.singleUserPermission.create !== 0 && (
           <SubNavbar
             Label={subNavConst.user.label}
             URL={subNavConst.user.link}
           />
-        )}
+        )
+      ) : (
+        <main className="flex min-h-screen flex-col items-center justify-between md:py-24">
+          <div className="flex items-center justify-center h-1/2 mt-32">
+            <div className="animate-pulse bg-gray-200 p-6 rounded-md shadow-md">
+              <div className="h-8 bg-gray-300 w-full mb-4">
+                <h6>Bvs System is Loading..................</h6>
+              </div>
+            </div>
+          </div>
+        </main>
+      )}
 
       <StaffTableContainer staffInfo={staffList} />
     </>
